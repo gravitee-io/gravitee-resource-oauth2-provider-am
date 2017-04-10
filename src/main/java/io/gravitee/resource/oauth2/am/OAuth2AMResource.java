222f106f5ee9efc8049f79453de5061c48096cde
brasseld <brasseld@gmail.com>
2017-04-10 16:50:51 +0200

    feat(oauth2): Gravitee.io AM OAuth2 resource
    Closes gravitee-io/issues#535

---
+++ src/main/java/io/gravitee/resource/oauth2/am/OAuth2AMResource.java
@@ -0,0 +1,120 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.resource.oauth2.am;
+
+import io.gravitee.common.http.HttpHeaders;
+import io.gravitee.common.http.HttpStatusCode;
+import io.gravitee.common.http.MediaType;
+import io.gravitee.gateway.api.handler.Handler;
+import io.gravitee.resource.oauth2.am.configuration.OAuth2ResourceConfiguration;
+import io.gravitee.resource.oauth2.api.OAuth2Resource;
+import io.gravitee.resource.oauth2.api.OAuth2Response;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpClientRequest;
+import io.vertx.core.http.HttpMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+import java.util.Base64;
+
+/**
+ * @author David BRASSELY (david.brassely at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class OAuth2AMResource extends OAuth2Resource<OAuth2ResourceConfiguration> {
+
+    private final Logger logger = LoggerFactory.getLogger(OAuth2AMResource.class);
+
+    private static final String HTTPS_SCHEME = "https";
+
+    private static final String AUTHORIZATION_HEADER_BASIC_SCHEME = "Basic ";
+    private static final char AUTHORIZATION_HEADER_VALUE_BASE64_SEPARATOR = ':';
+    private static final String CHECK_TOKEN_ENDPOINT = "/oauth/check_token?token=";
+
+    private HttpClient httpClient;
+
+    @Override
+    protected void doStart() throws Exception {
+        super.doStart();
+
+        logger.info("Starting an OAuth2 resource using Gravitee.io Access Management server at {}", configuration().getServerURL());
+        HttpClientOptions httpClientOptions = new HttpClientOptions();
+        httpClientOptions.setVerifyHost(false).setTrustAll(true);
+
+        httpClient = Vertx.vertx().createHttpClient(httpClientOptions);
+    }
+
+    @Override
+    protected void doStop() throws Exception {
+        super.doStop();
+
+        if (httpClient != null) {
+            httpClient.close();
+        }
+    }
+
+    @Override
+    public void introspect(String accessToken, Handler<OAuth2Response> responseHandler) {
+        OAuth2ResourceConfiguration configuration = configuration();
+        URI introspectionUri = URI.create(configuration.getServerURL());
+
+        final int port = introspectionUri.getPort() != -1 ? introspectionUri.getPort() :
+                (HTTPS_SCHEME.equals(introspectionUri.getScheme()) ? 443 : 80);
+
+        String introspectionEndpointURI = configuration.getServerURL() +
+                '/' +
+                configuration.getSecurityDomain() +
+                CHECK_TOKEN_ENDPOINT +
+                accessToken;
+
+        logger.debug("Introspect access token by requesting {} [{}]", introspectionEndpointURI);
+
+        HttpClientRequest request = httpClient.request(
+                HttpMethod.POST,
+                port,
+                introspectionUri.getHost(),
+                introspectionEndpointURI);
+
+        String authorizationValue = AUTHORIZATION_HEADER_BASIC_SCHEME +
+                    Base64.getEncoder().encodeToString(
+                            (configuration.getClientId() +
+                                    AUTHORIZATION_HEADER_VALUE_BASE64_SEPARATOR +
+                                    configuration.getClientSecret()).getBytes());
+        request.headers().add(HttpHeaders.AUTHORIZATION, authorizationValue);
+
+        // Set `Accept` header to ask for application/json content
+        request.headers().add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON);
+
+        request.handler(response -> response.bodyHandler(buffer -> {
+            logger.debug("AM Introspection endpoint returns a response with a {} status code", response.statusCode());
+            if (response.statusCode() == HttpStatusCode.OK_200) {
+                responseHandler.handle(new OAuth2Response(true, buffer.toString()));
+            } else {
+                responseHandler.handle(new OAuth2Response(false, buffer.toString()));
+            }
+        }));
+
+        request.exceptionHandler(event -> {
+            logger.error("An error occurs while checking access_token", event);
+            responseHandler.handle(new OAuth2Response(false, event.getMessage()));
+        });
+
+        request.end();
+    }
+}
